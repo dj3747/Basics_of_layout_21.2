f6345074a1d80e874d9e1002ed662488f030a79d
dj3747 <dj94911131@gmail.com>
2025-06-16 18:03:11 +0300

    fixed server.py

--- src/server.py
+++ src/server.py
@@ -6,35 +6,37 @@ PORT = 8000
 
 # Типы контента по расширению файла
 content_types = {
-    '.html': 'text/html',
-    '.css': 'text/css',
-    '.js': 'application/javascript',
-    '.png': 'image/png',
-    '.jpg': 'image/jpeg',
-    '.jpeg': 'image/jpeg'
+    ".html": "text/html",
+    ".css": "text/css",
+    ".js": "application/javascript",
+    ".png": "image/png",
+    ".jpg": "image/jpeg",
+    ".jpeg": "image/jpeg",
 }
 
 # Базовая директория скрипта
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
+
 class ContactPageHandler(http.server.BaseHTTPRequestHandler):
-    def do_GET(self):
+    def do_GET(self) -> None:
         # По умолчанию загружается index.html
-        rel_path = self.path[1:] or 'index.html'
+        rel_path = self.path[1:] or "index.html"
         ext = os.path.splitext(rel_path)[1]
-        content_type = content_types.get(ext, 'text/plain')
+        content_type = content_types.get(ext, "text/plain")
 
         abs_path = os.path.join(BASE_DIR, rel_path)
 
         try:
-            with open(abs_path, 'rb') as f:
+            with open(abs_path, "rb") as f:
                 data = f.read()
             self.send_response(200)
-            self.send_header('Content-Type', content_type)
+            self.send_header("Content-Type", content_type)
             self.end_headers()
             self.wfile.write(data)
         except FileNotFoundError:
             self.send_error(404, f"Файл {rel_path} не найден.")
+
 
 if __name__ == "__main__":
     with socketserver.TCPServer(("", PORT), ContactPageHandler) as httpd:
